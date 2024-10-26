06166a187f507f6c50632b305195a2898eb6f852
Manibharathi <manibharathidct@gmail.com>
2024-10-26 14:20:33 +0530

    try azure

---
+++ DevOpsTraning/Terraform/Try_azure/mani.tf
@@ -0,0 +1,33 @@
+provider "azurerm" {
+    // set the subscription id
+    subscription_id = var.subscription_id
+  features {}
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+resource "azurerm_resource_group" "mani-rg" {
+  name     = "mani-rg"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "mani-storage" {
+  name                     = "manistorageaccount"
+  resource_group_name      = azurerm_resource_group.mani-rg.name
+  location                 = azurerm_resource_group.mani-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_storage_container" "mani-container" {
+  name                  = "manicontainer"
+  storage_account_name  = azurerm_storage_account.mani-storage.name
+  container_access_type = "private"
+}
