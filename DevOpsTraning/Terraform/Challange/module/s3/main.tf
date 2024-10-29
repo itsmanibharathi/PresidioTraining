7c31739c5d8f143a11dadae5f2b2d2b39a4ee96d
Manibharathi <manibharathidct@gmail.com>
2024-10-29 11:24:35 +0530

    init

---
+++ DevOpsTraning/Terraform/Challange/module/s3/main.tf
@@ -0,0 +1,66 @@
+# Purpose: Create an S3 bucket with versioning, encryption, logging, public access block, and cross-account access policy.
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+  tags = {
+    name        = "${var.bucket_name}-bucket"
+  }
+}
+
+# Versioning
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration{
+    status = var.enable_versioning ? "Enabled" : "Suspended"
+  }
+}
+
+# server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "server_side_encryption" {
+  bucket = aws_s3_bucket.bucket.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = var.encryption_algorithm
+    }
+  }
+}
+
+
+# Logging
+resource "aws_s3_bucket_logging" "logging" {
+  bucket = aws_s3_bucket.bucket.id
+  target_bucket = var.logging_bucket
+  target_prefix = var.logging_prefix
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
+# # Cross-account access policy
+# resource "aws_s3_bucket_policy" "bucket_policy" {
+#   bucket = aws_s3_bucket.bucket.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Principal = {
+#           AWS = var.cross_account_iam_role_arns
+#         }
+#         Action = [
+#           "s3:GetObject",
+#           "s3:PutObject"
+#         ]
+#         Resource = [
+#           "${aws_s3_bucket.bucket.arn}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
