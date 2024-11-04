e9439a5c71e768a61cbcac12f817d23e017d37e7
Manibharathi <manibharathidct@gmail.com>
2024-11-04 11:17:55 +0530

    add checkov

--- checkov.sh
+++ checkov.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run Checkov to generate a JSON report
-checkov -d ./DevOpsTraning/Terraform/ -o json > checkov_report.json --no-cert-verify
+checkov -d ./DevOpsTraning/Terraform/Challange/ -o json > checkov_report.json --no-cert-verify
 
 # Run Python script to calculate compliance percentage
 python3 calculate_compliance.py
