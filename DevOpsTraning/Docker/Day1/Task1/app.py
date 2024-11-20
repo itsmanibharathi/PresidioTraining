4d15becc98f5bd3e891dcd01ac29250b5f5aae3f
Manibharathi <manibharathidct@gmail.com>
2024-11-20 14:57:05 +0530

    add day3 task

--- DevOpsTraning/Docker/Day1/Task1/app.py
+++ DevOpsTraning/Docker/Day1/Task1/app.py
@@ -1,13 +1,12 @@
-from flask import Flask, render_template_string
 import os
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    name = os.getenv("NAME", "World")
-    html = f"<h1>Hello, {name}!</h1>"
-    return render_template_string(html)
+@app.route('/')
+def index():
+    # Get the server name from the environment variable
+    server_name = os.getenv('SERVER_NAME', 'Not Set')
+    return f"Response from : {server_name}"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
